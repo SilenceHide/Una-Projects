488745db94b5193768d38834b99f34112988ff76
SilenceHide <silencehide.021@gmail.com>
2024-03-09 21:24:32 +0330

    Practice

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,31 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      "2xs": "440px",
+      xs: "540px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        Prompt: ["Prompt", "Arial", "sans-serif"],
+        PromptMed: ["Prompt Medium", "Arial", "sans-serif"],
+      },
+      colors: {
+        "main-bg-color": "#F9F6F5",
+        "main-text-color": "#32324D",
+        "secondary-text-color": "#AD8775",
+        "disable-text-color": "#EDE4E0",
+        "sub-text-color": "#666666",
+        "red-hot": "#FF6666",
+        "green-new": "#84BF56",
+        "orange-popular": "#E59D49",
+        "accent-color": "#DBA514",
+        "disable-color": "#D1BCB2",
       },
     },
   },
