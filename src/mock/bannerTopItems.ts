29bd7a2d956a6f50e2741a7b82960201524c0cbc
SilenceHide <silencehide.021@gmail.com>
2024-06-14 11:41:52 +0330

    making mock data

--- src/mock/bannerTopItems.ts
+++ src/mock/bannerTopItems.ts
@@ -1,5 +1,6 @@
 export const bannerTopItemsMock = [
   {
+    id: 5,
     title: "PENNY PENDANT LAMP SHADE",
     image: "/images/Banner/Chandelir1.webp",
     centerImage: "/images/Banner/Chandelir2.webp",
@@ -8,6 +9,7 @@ export const bannerTopItemsMock = [
     label: "hot",
   },
   {
+    id: 6,
     title: "EBRO BEDSIDE TABLE",
     image: "/images/Banner/Consool3.webp",
     centerImage: "/images/Banner/Consool2.webp",
@@ -16,6 +18,7 @@ export const bannerTopItemsMock = [
     label: "hot",
   },
   {
+    id: 8,
     title: "DAMIEN DINNER TABLE",
     image: "/images/Banner/DinnerTable1.webp",
     centerImage: "/images/Banner/DinnerTable2.webp",
