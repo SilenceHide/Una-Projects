11cf6ce72b8beac8fd07236cc8e61a84fef16216
SilenceHide <silencehide.021@gmail.com>
2024-06-18 22:16:53 +0330

    adding libraries

---
+++ src/api/config/ApiClient.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const apiClient = axios.create({
+  baseURL: "https://czigzag.navaxcollege.com",
+  timeout: 120000,
+});
+
+// apiClient.interceptors.request.use(function (request) {
+//     const token = window.localStorage.getItem("token");
+
+//     if (token) {
+//       request.headers.Authorization = "Bearer " + token;
+//     }
+
+//     return request;
+//   });
+
+apiClient.interceptors.response.use(
+  function (response) {
+    return response.data;
+  },
+  function (error) {
+    if (error.response) {
+      if (error.response.status === 404) {
+        toast.error("منابع درخواستی وجود ندارد");
+      } else if (error.response.status === 400) {
+        toast.error("پارامترهای ارسالی صحیح نمی‌باشند");
+      } else if (error.response.status === 401) {
+        toast.error("لطفا وارد شوید");
+      } else if (error.response.status === 403) {
+        toast.error("شما دسترسی به این منابع ندارید");
+      } else {
+        toast.error("خطایی رخ داده است");
+      }
+    } else if (error.request) {
+      toast.error("ارتباط با سرور برقرار نیست");
+      console.log(error.request);
+    } else {
+      toast.error("خطای نامعلوم");
+      console.log("Error", error.message);
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default apiClient;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,33 @@ import Layout from "@/components/layouts/Layout";
 import "@/styles/icons.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchIntervalInBackground: false,
+        retry: 0,
+      },
+    },
+  });
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+        <ToastContainer
+          // autoClose={true}
+          hideProgressBar={false}
+          closeOnClick={true}
+          draggable={false}
+          theme="light"
+          position="top-right"
+        />
+      </Layout>
+    </QueryClientProvider>
   );
 }
